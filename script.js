02c78e8b43bb15277bb861c1319c74b3163828cd
Edwin Reynoso <eorroe@gmail.com>
2015-10-10 07:47:42 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,19 +1,17 @@
 (function() {
-  function magic(prop) {
-    try {
-      var oldFunc = HTMLElement.prototype[prop];
-      if(oldFunc instanceof Function) {
-        HTMLElement.prototype[prop] = function() {
-          var args = arguments, target = this;
-          return new Promise(function(res, rej) {
-            setTimeout(function() {
-              res ( oldFunc.apply( target, args ) );
-            }, 0);
-          });
-        }
-      }
-    } catch(e) {}
-  }
+	function magic( prop ) {
+		var oldFunc = div[ prop ];
+		if( oldFunc instanceof Function ) {
+			HTMLElement.prototype[prop] = function() {
+				var args = arguments, target = this;
+				return new Promise(function( res, rej ) {
+					res ( oldFunc.apply( target, args ) );
+				});
+			}
+		}
+	}
 
-  for(var prop in HTMLElement.prototype) magic(prop);
+	var div = document.createElement( 'div' );
+	for( var prop in div ) magic( prop );
+	div = prop = null;
 })();
